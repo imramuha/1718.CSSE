c830815afd8dee937283c47d81a0ee11850ee491
imranmuha <imramuha@student.arteveldehogeschool.be>
2017-10-03 14:58:32 +0200

    test
    test

---
+++ Week02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Week02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
